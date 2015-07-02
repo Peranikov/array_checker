3fd12a97fe0447a1f67ebd47b553d803b955f296
Peranikov <ym.nekoze@gmail.com>
2015-07-02 21:43:43 +0900

    Add unit tests

--- spec/array_checker_spec.rb
+++ spec/array_checker_spec.rb
@@ -17,4 +17,18 @@ describe 'ArrayChecker' do
       expect(checker.find_not_exist_number).to eq 9999
     end
   end
+
+  describe '#find_not_exist_number' do
+    it '1 ~ 10000のうち、1が存在しない場合' do
+      checker = ArrayChecker.new("./spec/fixtures/not_number_1")
+      expect(checker.find_not_exist_number).to eq 1
+    end
+  end
+
+  describe '#find_not_exist_number' do
+    it '1 ~ 10000のうち、10000が存在しない場合' do
+      checker = ArrayChecker.new("./spec/fixtures/not_number_10000")
+      expect(checker.find_not_exist_number).to eq 10000
+    end
+  end
 end
