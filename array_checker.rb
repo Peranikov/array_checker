fd7595df3829e804ec405fc29af4248ab0d1ea9a
Peranikov <ym.nekoze@gmail.com>
2015-07-02 21:53:23 +0900

    Refactoring

--- array_checker.rb
+++ array_checker.rb
@@ -1,14 +1,18 @@
 class ArrayChecker
   def initialize(path, range = 1..10000)
-    @array = convert_to_array(path)
-    @range_sum = range.reduce(:+) # 1..10000固定であれば50005000の定数で良い
+    @path = path
+    @range = range
   end
 
   def find_not_exist_number
-    @range_sum - @array.reduce(:+)
+    range_sum - convert_to_array(@path).reduce(:+)
   end
 
   private
+
+  def range_sum
+    @range.reduce(:+) # 1..10000固定であれば50005000の定数で良い
+  end
 
   def convert_to_array(path)
     file = File.read(path)
--- spec/array_checker_spec.rb
+++ spec/array_checker_spec.rb
@@ -1,31 +1,27 @@
 require './array_checker'
 
 describe 'ArrayChecker' do
-  describe '#new' do
+  describe '#find_not_exist_number' do
     it 'ファイルが存在しない場合' do
-      expect {ArrayChecker.new("./spec/fixtures/dummy")}.to raise_error Errno::ENOENT
+      checker = ArrayChecker.new("./spec/fixtures/dummy")
+      expect {expect(checker.find_not_exist_number)}.to raise_error Errno::ENOENT
     end
 
     it 'ファイルが配列の形式でない場合' do
-      expect {ArrayChecker.new("./spec/fixtures/not_array")}.to raise_error ArgumentError
+      checker = ArrayChecker.new("./spec/fixtures/not_array")
+      expect {expect(checker.find_not_exist_number)}.to raise_error ArgumentError
     end
-  end
 
-  describe '#find_not_exist_number' do
     it '1 ~ 10000のうち、9999が存在しない場合' do
       checker = ArrayChecker.new("./spec/fixtures/not_number_9999")
       expect(checker.find_not_exist_number).to eq 9999
     end
-  end
 
-  describe '#find_not_exist_number' do
     it '1 ~ 10000のうち、1が存在しない場合' do
       checker = ArrayChecker.new("./spec/fixtures/not_number_1")
       expect(checker.find_not_exist_number).to eq 1
     end
-  end
 
-  describe '#find_not_exist_number' do
     it '1 ~ 10000のうち、10000が存在しない場合' do
       checker = ArrayChecker.new("./spec/fixtures/not_number_10000")
       expect(checker.find_not_exist_number).to eq 10000
